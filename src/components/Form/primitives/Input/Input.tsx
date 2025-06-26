cf4296cfa48b1f667af9140044b4912ab2737d8b
Tomas Padilla <padillatomasagustin@gmail.com>
2025-06-26 19:41:01 +0200

    [ACUI-4] Basic Input and story with styles

--- src/components/Form/primitives/Input/Input.tsx
+++ src/components/Form/primitives/Input/Input.tsx
@@ -4,17 +4,7 @@ import { clsx } from 'clsx';
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type = 'text', ...props }, ref) => {
-  return (
-    <input
-      ref={ref}
-      type={type}
-      className={clsx(
-        'block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-primary focus:border-primary sm:text-sm',
-        className
-      )}
-      {...props}
-    />
-  );
+  return <input ref={ref} type={type} className={clsx('input-primary', className)} {...props} />;
 });
 
 Input.displayName = 'Input';
