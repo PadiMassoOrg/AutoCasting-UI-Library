120b013c1d063887480d82393ad10a97e910c479
Tomas Padilla <padillatomasagustin@gmail.com>
2025-06-29 10:13:00 +0200

    [ACUI-4] small storybook change

--- src/components/Form/Fields/FormInputField/FormInputField.stories.tsx
+++ src/components/Form/Fields/FormInputField/FormInputField.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import FormInputField from './FormInputField';
 
 const meta: Meta<typeof FormInputField> = {
-  title: 'Form/Field/FormInputField',
+  title: 'Form/FormInputField',
   component: FormInputField,
   tags: ['autodocs'],
 };
