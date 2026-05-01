2330cf96218a2649621cd97dd63440409c851ada
Tomas Padilla <padillatomasagustin@gmail.com>
2026-05-01 13:02:13 +0200

    [ACIT-51] FIX TEMPORAL: MAX WIDTH y deploy

--- src/components/Layout/Dashboard/DashboardShell.tsx
+++ src/components/Layout/Dashboard/DashboardShell.tsx
@@ -204,7 +204,7 @@ function DashboardShell<Key extends string = string>({
   return (
     <DashboardShellContext.Provider value={ctxValue}>
       <section
-        className="box-border w-full flex flex-col bg-(--color-secondary-white) lg:px-10 lg:py-6"
+        className="box-border w-full max-w-[1650px] m-auto flex flex-col bg-(--color-secondary-white) lg:px-10 lg:py-6"
         style={isDesktop ? { minHeight: desktopViewportHeight } : undefined}
       >
         {/* Desktop Title */}
